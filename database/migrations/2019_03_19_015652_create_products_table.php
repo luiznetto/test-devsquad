67b2daa3bd1c75497550ec3ac23cb42abe64848d
Luiz Neto <lu_iz93@hotmail.com>
2019-03-18 23:24:17 -0300

    Created model and migration to Category and Product.

---
+++ database/migrations/2019_03_19_015652_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description');
+            $table->string('image');
+            $table->double('price', 8, 2);
+            $table->integer('category_id');
+
+            $table->timestamps();
+
+            $table
+                ->foreign('category_id')
+                ->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
